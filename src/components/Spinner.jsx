44915271b836ed27e6c55b413a952cb42ea17ff6
Joshua Atendido Bear <joshuabear87@gmail.com>
2025-03-16 18:07:38 -0700

    Create, Update, Delete Books

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Spinner = () => {
   return (
-    <div className='animate-ping w-16 h-16 m-8 bg-sky-600'>
-      
+    <div className="flex justify-center items-top h-screen">
+      <div className='animate-ping w-16 h-16 m-8 rounded-full bg-sky-600'></div>
     </div>
   )
 }
