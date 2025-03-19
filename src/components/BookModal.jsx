5803edce1e015fefd34c09036e6b87a0f4963285
Joshua Atendido Bear <joshuabear87@gmail.com>
2025-03-19 08:04:59 -0700

    completed app

--- src/components/BookModal.jsx
+++ src/components/BookModal.jsx
@@ -5,7 +5,7 @@ import { BiUserCircle } from 'react-icons/bi';
 const BookModal = ({ book, onClose }) => {
   return (
     <div 
-      className='fixed bg-black bg-opacity-60 top-0 left-0 right-0 bottom-0 z-50 flex justify-center items-center'
+      className='fixed bg-black bg-opacity-25 top-0 left-0 right-0 bottom-0 z-50 flex justify-center items-center'
       onClick={onClose}
     >
       <div
@@ -13,7 +13,7 @@ const BookModal = ({ book, onClose }) => {
         className='w-[600px] max-w-full h-[400px] bg-white rounded-xl p-4 flex flex-col relative'
       >
         <AiOutlineClose 
-          className='absolute right-6 top-6 text-3xl text0red0600 cursor-pointer'
+          className='absolute right-6 top-6 text-3xl text-red-600 cursor-pointer'
           onClick={onClose}
         />  
         <h2 className='w-fit px-4 py-1 bg-red-300 rounded-lg'>
