4a5da1f3d98221d95e554388a09fef069d210b8e
Joshua Atendido Bear <joshuabear87@gmail.com>
2025-03-25 14:55:47 -0700

    troubleshooting 5

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get(`${API_URL}/books`)
+      .get(`${API_URL}`)
       .then((res) => {
         setBooks(res.data);
         setLoading(false);
